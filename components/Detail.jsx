2d163b9dc891c84e81c87f72eb384173f3c6c40e
Çağla Pekoğlu <cagla.pekoglu@spacegoatstudios.com>
2023-10-03 19:24:07 +0300

    .

--- components/Detail.jsx
+++ components/Detail.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Detail = () => {
   return (
     <div className={styles.container}>
-        <h1>İstanbul bölgesinde saygın bir hukuk firması olarak,İğdeci Aytekin Hukuk& Danışmanlık Bürosu, adaleti ve müşteri memnuniyetini en üst düzeye çıkarmak için çalışır. Misyonumuz, müvekkillerimize dürüstlük, bilgi ve adanmışlıkla hukuki sorunlarını çözmelerine yardımcı olmaktır.</h1>
+        <h1>Uzman avukat ve danışman kadromuz, İğdeci Aytekin'i güçlü kılan unsurlardan biridir. Müvekkillerimizin karşılaştığı zorlukları anlayarak, hukuki meselelerin her birinin benzersiz gereksinimlerine odaklanıyor ve müvekkil odaklı yaklaşımımızı her zaman önceliğimiz olarak koruyoruz.</h1>
 
         <Link href="/hakkimizda">
           Detaylı Bilgi
